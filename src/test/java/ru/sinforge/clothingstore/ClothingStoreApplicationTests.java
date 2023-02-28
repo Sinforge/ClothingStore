1aaa16c3dfe6da8e1958e335bdb9d473a438683b
Vlad <vlad.vlasov77@mail.ru>
2023-02-28 13:37:29 +0300

    Initial commit

---
+++ src/test/java/ru/sinforge/clothingstore/ClothingStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.sinforge.clothingstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClothingStoreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
